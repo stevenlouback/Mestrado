7278001909602686fe816b858a6e95c2df3645ed
marlonsFSantos <marlonsfsantos@gmail.com>
2019-07-08 18:06:31 -0300

    Ajustes amostra
    RGB, redimensionamento,etc

--- api-Regressao-chemo/ProjetoJSF/ChemoAPP/src/main/java/com/mf2solucoes/application/beans/cargaIsoladaBean.java
+++ api-Regressao-chemo/ProjetoJSF/ChemoAPP/src/main/java/com/mf2solucoes/application/beans/cargaIsoladaBean.java
@@ -5,6 +5,7 @@
  */
 package com.mf2solucoes.application.beans;
 
+import com.google.gson.Gson;
 import com.mf2solucoes.application.modelDb.amostra;
 import com.mf2solucoes.application.modelDb.modelo;
 import com.mf2solucoes.application.modelDb.parametro;
@@ -12,14 +13,21 @@ import com.mf2solucoes.application.repository.amostras;
 import com.mf2solucoes.application.repository.modelos;
 import com.mf2solucoes.application.repository.parametros;
 import com.mf2solucoes.application.service.amostraService;
-import com.mf2solucoes.application.service.modeloService;
 import com.mf2solucoes.tools.Mensagens;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.Raster;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Base64;
+//import java.util.Base64;
 import java.util.Date;
 import java.util.List;
-import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
+import javax.imageio.ImageIO;
 //import javax.faces.view.ViewScoped;
 //import javax.faces.bean.ViewScoped;
 import org.omnifaces.cdi.ViewScoped;
@@ -27,6 +35,8 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import lombok.Getter;
 import lombok.Setter;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
 
 /**
  *
@@ -45,7 +55,11 @@ public class cargaIsoladaBean implements Serializable {
     @Setter
     @Getter
     private amostra amostra;
-    
+
+    @Setter
+    @Getter
+    private BufferedImage image;
+
     @Setter
     @Getter
     private parametro parametroResultado;
@@ -70,7 +84,7 @@ public class cargaIsoladaBean implements Serializable {
 
     @Inject
     private amostras amostras;
-    
+
     @Inject
     private amostraService amostraService;
 
@@ -102,12 +116,14 @@ public class cargaIsoladaBean implements Serializable {
 //            return false;
 //        }
 //        return amostra.getModelo().getTpinstrumento().equals("IMG");
-return false;
+        return false;
     }
 
     private void limpar() {
         modelos = new modelos();
         modelo = new modelo();
+        amostras = new amostras();
+        amostra = new amostra();
     }
 
     public void preencheCombo1() {
@@ -130,40 +146,107 @@ return false;
         param.setModelo(modelo);
         list_ParametroResultado = parametros.findParametrosModelo(param);
     }
-    
-    public void gerarAmostra(){
+
+    public void gerarAmostra() {
         Mensagens msg = new Mensagens();
         try {
             amostraService = new amostraService();
-            
-            if (modelo.getNmmodelo().equals("")){
-                msg.addError("model.validation.name", modelo);
+
+            if (modelo.getNmmodelo().equals("")) {
+                msg.addError("model.validation.name", amostra);
                 return;
             }
             
-            if (modelo.getTpinstrumento().equals("")){
-                msg.addError("model.validation.instrumento", modelo);
+            if (amostra.getDtcoletaamostra().equals("")){
+                msg.addError("amostra.validation.dataamostra", amostra);
                 return;
             }
             
-            if (modelo.getNmmetodoreferencia().equals("")){
-                msg.addError("model.validation.metodo", modelo);
+            if (amostra.getTpamostra().equals("")){
+                msg.addError("amostra.validation.identifica", amostra);
+                return;
+            }
+
+            if (amostra.getDsobservacoes().equals("")){
+                msg.addError("amostra.validation.obs", amostra);
                 return;
             }
             
-            if (modelo.getDsmodelo().equals("")){
+            if (modelo.getNmmetodoreferencia().equals("NIR")) {
+                if (amostra.getDsespectro().equals("")) {
+                    msg.addError("amostra.valida.espectro", amostra);
+                    return;
+                }
+            } else {
+                if (amostra.getImamostra().equals("")) {
+                    msg.addError("amostra.valida.imagem", amostra);
+                    return;
+                }
+            }
+
+            if (modelo.getDsmodelo().equals("")) {
                 msg.addError("model.validation.descricao", modelo);
                 return;
             }
-            
+
             this.amostra = amostraService.salvar(amostra);
             limpar();
             msg.addInfo("saved", "");
         } catch (Exception e) {
-            
+
             msg.addError(String.valueOf(e), amostra);
             e.printStackTrace();
         }
+    }
+
+    public void converteImageRgb(FileUploadEvent event) {
+        Raster raster;
+        try {
+            byte[] bimagem = event.getFile().getContents();
+
+            ByteArrayInputStream bais = new ByteArrayInputStream(bimagem);
+            image = ImageIO.read(bais);
+
+            image = redimensionaImg(image, 15, 15);
+
+            //Inclui a imagem redimensionada no Array de Byte
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(image, "jpg", baos);
+            baos.flush();
+            byte[] imageInByte = baos.toByteArray();
+            baos.close();
+            amostra.setImagem(imageInByte);
+            //Tratamento para jogar em um array 64 bits
+            String imgArray = Base64.getEncoder().encodeToString(amostra.getImagem());
+            amostra.setImamostra(imgArray);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        //Percorre a Imagem pegando os RGB
+        raster = image.getRaster();
+        int cores[] = new int[255];
+        // esse vetor "cores[]" vai armazenar as cores RGB  
+        // [0] será Red(R), [1] será Green(G) e [2] será Blue(B)  
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                raster.getPixel(x, y, cores); // captura da combinação de cor do pixel  
+                System.out.println("R(" + cores[0] + ") G(" + cores[1] + ") B(" + cores[2] + ")");
+            }
+        }
+        // esses laços varrem todo o objeto "imagem",   
+        // saindo do eixo 0,0 até o último pixel da imagem 
+    }
+
+    public BufferedImage redimensionaImg(BufferedImage imagem, int new_w, int new_h) throws IOException {
+        //BufferedImage imagem = ImageIO.read(image);
+        BufferedImage new_img = new BufferedImage(new_w, new_h, BufferedImage.TYPE_INT_RGB);
+
+        Graphics2D g = new_img.createGraphics();
+        g.drawImage(imagem, 0, 0, new_w, new_h, null);
+        g.dispose();
+
+        return new_img;
     }
 
 }
